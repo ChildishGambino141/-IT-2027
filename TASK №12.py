0120aa09c324a9591ab7b2dc7637f96ef1825879
ChildishGambino141 <kuryshkin09@gmail.com>
2026-02-08 16:36:12 +0300

    new commit

---
+++ TASK №12.py
@@ -0,0 +1,14 @@
+maxi=0
+for n in range(3,101):
+    s="2"+"8"*n
+    while "82" in s or "888" in s or "6666" in s:
+        if "82" in s:
+            s=s.replace('82',"28",1)
+        if "888" in s:
+            s=s.replace('888',"62",1)
+        if "6666" in s:
+            s=s.replace('6666',"6",1)
+    if s.count("6")==8:
+        maxi=max(n,maxi)
+print(maxi)   
+''' ответ: 26'''                    
